05d03416f6df728410cb8d0653657c01e1c888f3
William Bikuta <william.bikuta@he-arc.ch>
2022-05-27 18:41:25 +0200

    add run animation

---
+++ Projektion/Assets/Player/Scripts/AnimatorManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorManager : MonoBehaviour
+{
+    private Animator playerAnimator;
+    private InputManager inputManager;
+
+
+    private void Awake()
+    {
+        playerAnimator = GetComponent<Animator>();
+        inputManager = GetComponent<InputManager>();
+    }
+
+    private void Update()
+    {
+        if(inputManager.verticalInput != 0 || inputManager.horizontalInput != 0)
+        {
+            playerAnimator.SetBool("isRunning", true);
+        }
+        else
+        {
+            playerAnimator.SetBool("isRunning", false);
+        }
+    }
+}
